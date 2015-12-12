231482df7f56445f1072c798455a187d23a018a5
Grégory Paul <paulgreg@gmail.com>
2015-12-12 16:54:14 +0100

    Respond to send intent

--- app/app.js
+++ app/app.js
@@ -8,7 +8,16 @@ if (application.android) {
     application.android.on(application.AndroidApplication.activityCreatedEvent, function (args) {
         if (args.activity.getIntent) {
             var intent = args.activity.getIntent();
-            var url = intent.getData();
+            var url;
+
+            if (intent.getAction() == android.content.Intent.ACTION_VIEW) {
+                url = intent.getData(); // VIEW intent
+            } else if (intent.getAction() == android.content.Intent.ACTION_SEND) {
+                if (intent.getStringExtra) {
+                    url = intent.getStringExtra(android.content.Intent.EXTRA_TEXT);
+                }
+            }
+
             if (url) {
                 // Using application setting to send data to main-page... (surely overkill but haven’t found a better way yet...)
                 applicationSettings.setString("startUrl", url.toString());
